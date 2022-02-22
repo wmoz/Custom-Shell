85e4a624cfa525307c0d5a476c60821d1518a4f5
Omar Ahmed Elgeoushy <omarelgeoushy@sumac.rlogin>
2022-02-22 17:56:25 -0500

    Edited the test files to match what we are testing

--- tests/custom.tst
+++ tests/custom.tst
@@ -1,2 +1,4 @@
 = custom Tests
 5 custom/test_history.py
+5 custom/test_history1.py
+5 custom/test_prompt.py
--- tests/custom/test_history.py
+++ tests/custom/test_history.py
@@ -2,26 +2,11 @@
 #     1. making sure it prints out the history as expected
 #     2. making sure that commands such as !! and !l work correctly
 
-import sys, imp, atexit, pexpect, signal, time, threading
+#import sys, imp, atexit, pexpect, signal, time, threading
 from testutils import *
 
 setup_tests()
 
 expect_prompt()
-sendline('ls')
-
-expect_prompt()
-sendline('sleep 1')
-
-expect_prompt()
 sendline('history')
-expect('   0 ls\n   1 sleep 1\n   2 history', 'first history didnt pass') #something goes in here to check output with the expected
-
-# expect_prompt()
-# sendline('!!')
-# expect() #something goes in here to check output with the expected
-
-# expect_prompt()
-# sendline('!l')
-# expect() #something goes in here to check output with the expected
-
+expect('   0 history', 'first history didnt pass')

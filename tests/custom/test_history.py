1d8c8a6dedcdc286cc6ef0c1fa98797e7eb4a869
Omar Ahmed Elgeoushy <omarelgeoushy@perisimmon.rlogin>
2022-02-23 12:06:32 -0500

    Commented the test files and fixed the test prompt

--- tests/custom/test_history.py
+++ tests/custom/test_history.py
@@ -1,11 +1,11 @@
 # Test class for the more complex builtin "history"
 #     1. making sure it prints out the history as expected
 
-#import sys, imp, atexit, pexpect, signal, time, threading
 from testutils import *
 
 setup_tests()
 
+#sending history to an empty list to make sure it prints correctly
 expect_prompt()
 sendline('history')
 expect('   0 history', 'first history didnt pass')
--- tests/custom/test_prompt.py
+++ tests/custom/test_prompt.py
@@ -1,8 +1,24 @@
 #Tests the custome prompt to make sure it meets the requirement 
 
+from getpass import getuser
 import os
+import pwd
+import socket
 from testutils import *
 
 setup_tests()
 
-expect_prompt()
+#get the hostname
+hostname = socket.gethostname()
+user = pwd.getpwuid(os.getuid())[0]
+
+#get the path
+path = os.getcwd()
+#get the current directory only
+cwd = os.path.basename(path)
+
+#expected string
+expected = '<' + user + '@' + hostname + ' ' + cwd + '>$ '
+
+expect_exact(expected, 'error didnt pass')
+test_success()
